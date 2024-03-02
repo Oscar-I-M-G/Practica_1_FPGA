17c44f9b961fc6330ab4e924d2222e938fabdcf5
Oscar-I-M-G <124809856+Oscar-I-M-G@users.noreply.github.com>
2024-03-02 16:13:34 +0100

    yeas

--- Practica_Tarea_3/simulation/qsim/work/contador_2_vlg_sample_tst/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity contador_2_vlg_sample_tst is
-    port(
-        CLOCK           : in     vl_logic;
-        RST             : in     vl_logic;
-        sampler_tx      : out    vl_logic
-    );
-end contador_2_vlg_sample_tst;
--- Practica_Tarea_3/simulation/qsim/work/contador_2_vlg_vec_tst/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity contador_2_vlg_vec_tst is
-end contador_2_vlg_vec_tst;
